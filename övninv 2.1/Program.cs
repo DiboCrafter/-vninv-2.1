82434d228f149ae42c356c2323f049b5e3dbe8a0
DiboCrafter <emsv06002@utb.vaxjo.se>
2024-11-14 12:18:40 +0100

    Add project files.

---
+++ övninv 2.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace övning2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej! Vilken är din favorite bok?");
+            string x = Console.ReadLine();
+            Console.WriteLine("okej så din favorite bok är" + " " + x + " " + "jag gillar den också.");
+        }
+    }
+}
